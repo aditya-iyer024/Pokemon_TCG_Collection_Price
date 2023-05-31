5061afba45f57271ecfb20b68d2bde3adbe3dca3
Aditya Iyer <72334855+aditya-iyer024@users.noreply.github.com>
2023-05-31 18:12:22 +1000

    Updated the file to be in the form of a function

--- getcards.py
+++ getcards.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import re
+from datetime import date
 
 # Function to convert the text to a dictionary
 def get_cards(card_string):
     # Define regular expressions for each field
-    name_pattern = r'^([^\[]+)' 
+    name_pattern = r'(.+?) #'
     type_pattern = r'\[(.*?)\]'
     number_pattern = r'#(\d+)'
     series_pattern = r'Pokemon (.+)'
@@ -15,7 +16,7 @@ def get_cards(card_string):
     if name_match:
         name = name_match.group(1) # If there is [] in the name
     else:
-        name_pattern = r'(.+?) #' # If the is no [] in the name
+        name_pattern = r'^([^\[]+)' # If the is no [] in the name
         name = re.search(name_pattern, card_string)
     number = re.search(number_pattern, card_string).group(1)
     card_type_match = re.search(type_pattern, card_string)
@@ -36,27 +37,33 @@ def get_cards(card_string):
 
     return(card_data)
 
-url = 'https://www.pricecharting.com/offers?seller=7ugl5zx7u5td7l447wcxgbn6vy&status=collection' #Update the URL
+def get_card_list():
+    url = 'https://www.pricecharting.com/offers?seller=7ugl5zx7u5td7l447wcxgbn6vy&status=collection' #Update the URL
 
-# Read HTML table into a list of DataFrames
-dfs = pd.read_html(url)
+    # Read HTML table into a list of DataFrames
+    dfs = pd.read_html(url)
 
-# Select the DataFrame which contains the Pokemon card data
-df = dfs[1]['Item']
+    # Select the DataFrame which contains the Pokemon card data
+    df = dfs[1]['Item']
 
-# Define the final DataFrame
-card_list = pd.DataFrame(columns=['Name', 'Type', 'Number', 'Series', 'Grade'])
+    # Define the final DataFrame
+    card_list = pd.DataFrame(columns=['Name', 'Type', 'Number', 'Series', 'Grade'])
 
-# Iterate through the items that contain the card data
-for i in range(1,len(df),2):
-    # Filters the items that are not a float item, this is because the 'float' items are 'nan' 
-    if type(df[i]) != float:
-        row = pd.Series(get_cards(df[i]))
-        # Adds the series item as a new row to the final DataFrame
-        row['Value'] = float(dfs[1]['Value'][i][1:5])
-        row['Grade'] = (dfs[1]['Includes / Qty'][i])
-        card_list = pd.concat([card_list, row.to_frame().T], ignore_index=True)
+    # Get the date for the present value
+    today = date.today().strftime('%d%m%y')
+    # Iterate through the items that contain the card data
+    for i in range(1,len(df),2):
+        # Filters the items that are not a float item, this is because the 'float' items are 'nan' 
+        if type(df[i]) != float:
+            row = pd.Series(get_cards(df[i]))
+            # Adds the series item as a new row to the final DataFrame
+            try:
+                row['Value('+today+')'] = float(dfs[1]['Value'][i][1:5])
+            except:
+                row['Value('+today+')'] = 0
+            row['Grade'] = (dfs[1]['Includes / Qty'][i])
+            card_list = pd.concat([card_list, row.to_frame().T], ignore_index=True)
 
-# Save the table to a file        
-# card_list.to_json('card_list.json')
-card_list.to_csv('card_list.csv')
+    # Save the table to a file        
+    # card_list.to_json('card_list.json')
+    card_list.to_csv('card_list.csv')
